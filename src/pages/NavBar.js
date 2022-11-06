45b20d8489bd0a23c05140752f662c5643d0cc7a
Bagas <mbagas221@gmail.com>
2022-11-06 22:29:38 +0700

    update

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -327,7 +327,7 @@ function Navigation({ totalItems, emptyCart }) {
                   border: "none",
                 }}
               >
-                {state.user.image === "http://localhost:5000/uploads/" ? (
+                {state.user.image === "https://res.cloudinary.com/dy5ntbnnh/image/upload/v1667747909/waysfood/" ? (
                   <img
                     src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
                     alt="logo"
@@ -385,7 +385,7 @@ function Navigation({ totalItems, emptyCart }) {
                   border: "none",
                 }}
               >
-                {state.user.image === "http://localhost:5000/uploads/" ? (
+                {state.user.image === "https://res.cloudinary.com/dy5ntbnnh/image/upload/v1667747909/waysfood/" ? (
                   <img
                     src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
                     alt="logo"
