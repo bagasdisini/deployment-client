ec910401b174efd7cf18374506359b11b5c9de4c
Bagas <mbagas221@gmail.com>
2022-11-07 00:27:24 +0700

    update

--- src/pages/CartOrder.js
+++ src/pages/CartOrder.js
@@ -85,9 +85,11 @@ function Cart({
     location: `${position.lat}, ${position.lng}`,
   });
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
     try {
-      const body = JSON.stringify(form);
+      e.preventDefault();
+
+      // const body = JSON.stringify(form);
 
       const data = await API.post("/transaction", form, {
         headers: {
@@ -95,24 +97,24 @@ function Cart({
         },
       });
 
-      const token = data.data.token;
+      // const token = data.data.token;
 
-      window.snap.pay(token, {
-        onSuccess: function (result) {
-          console.log(result);
-          navigate("/my-profile");
-        },
-        onPending: function (result) {
-          console.log(result);
-          navigate("/my-profile");
-        },
-        onError: function (result) {
-          console.log(result);
-        },
-        onClose: function () {
-          alert("you closed the popup without finishing the payment");
-        },
-      });
+      // window.snap.pay(token, {
+      //   onSuccess: function (result) {
+      //     console.log(result);
+      //     navigate("/my-profile");
+      //   },
+      //   onPending: function (result) {
+      //     console.log(result);
+      //     navigate("/my-profile");
+      //   },
+      //   onError: function (result) {
+      //     console.log(result);
+      //   },
+      //   onClose: function () {
+      //     alert("you closed the popup without finishing the payment");
+      //   },
+      // });
 
       emptyCart();
     } catch (error) {
@@ -120,19 +122,19 @@ function Cart({
     }
   };
 
-  useEffect(() => {
-    const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
-    const myMidtransClientKey = "SB-Mid-client-13cP6QJtzCgJ6ixQ";
+  // useEffect(() => {
+  //   const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
+  //   const myMidtransClientKey = "SB-Mid-client-13cP6QJtzCgJ6ixQ";
   
-    let scriptTag = document.createElement("script");
-    scriptTag.src = midtransScriptUrl;
-    scriptTag.setAttribute("data-client-key", myMidtransClientKey);
+  //   let scriptTag = document.createElement("script");
+  //   scriptTag.src = midtransScriptUrl;
+  //   scriptTag.setAttribute("data-client-key", myMidtransClientKey);
   
-    document.body.appendChild(scriptTag);
-    return () => {
-      document.body.removeChild(scriptTag);
-    };
-  }, []);
+  //   document.body.appendChild(scriptTag);
+  //   return () => {
+  //     document.body.removeChild(scriptTag);
+  //   };
+  // }, []);
 
   return (
     <div>
@@ -336,6 +338,7 @@ function Cart({
                 className="px-5"
                 onClick={() => {
                   handleSubmit();
+                  navigateProfile();
                 }}
               >
                 Order!
